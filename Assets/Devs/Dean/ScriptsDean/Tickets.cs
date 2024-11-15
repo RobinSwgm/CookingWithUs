f3bb52a3e02731309f49bc6c3022bdc5da4bba82
DeanRBLagas <deanlagas@gmail.com>
2024-11-15 13:09:53 +0100

    Created a few temporary prefabs to then put into the UI canvas.

--- Assets/Devs/Dean/ScriptsDean/Tickets.cs
+++ Assets/Devs/Dean/ScriptsDean/Tickets.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class Tickets : MonoBehaviour
 {
-    public GameObject burgerBottomBunImage;
-    public GameObject burgerTopBunImage;
-    public GameObject burgerMeatImage;
+    public GameObject burgerBottomBun;
+    public GameObject burgerTopBun;
+    
+    public List<GameObject> burgerTicketList;
+    public List<GameObject> burgerIngredientList;
+
+    public TextMeshProUGUI ticketList;
 
     public int difficulty = 1;
 
@@ -17,26 +22,50 @@ public class Tickets : MonoBehaviour
     private void Start()
     {
         CreateTicket();
+        UpdateTicketList();
+        CreateBurgerTicket();
     }
 
     private void CreateTicket()
     {
         ticket = new List<string>();
         ticket.Add("BottomBun");
-        int size = Random.Range(1, difficulty);
+        int size = Random.Range(1, difficulty + 1);
         for (int i = 0; i < size; i++)
         {
             int choice = Random.Range(0, ingredients.Count);
             ticket.Add(ingredients[choice]);
-            Debug.Log(choice);
         }
         ticket.Add("TopBun");
+    }
 
-        if (ticket[0] == "BottomBun" && ticket[1] == "BurgerMeat" && ticket[ticket.Count - 1] == "TopBun")
+    private void UpdateTicketList()
+    {
+        ticketList.text = "";
+
+        for (int i = 0;i < ticket.Count;i++)
         {
-            burgerBottomBunImage.SetActive(true);
-            burgerMeatImage.SetActive(true);
-            burgerTopBunImage.SetActive(true);
+            ticketList.text += $"{ticket[i]}\n";
         }
+    }
+
+    private void CreateBurgerTicket()
+    {
+        burgerTicketList = new List<GameObject>();
+        burgerTicketList.Add(burgerBottomBun);
+
+        int size = Random.Range(1, difficulty + 1);
+        for (int i = 0; i < size; i++)
+        {
+            int choice = Random.Range(0, burgerIngredientList.Count);
+            burgerTicketList.Add(burgerIngredientList[choice]);
+        }
+
+        burgerTicketList.Add(burgerTopBun);
+    }
+
+    private void UpdateBurgerTicket()
+    {
+
     }
 }
