f56497b8aa01249513c77016e7620d1b81f178da
DeanRBLagas <deanlagas@gmail.com>
2024-11-19 13:28:19 +0100

    Updated the tickets

--- Assets/Devs/Dean/ScriptsDean/Tickets.cs
+++ Assets/Devs/Dean/ScriptsDean/Tickets.cs
@@ -1,85 +1,179 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class Tickets : MonoBehaviour
 {
     public GameObject burgerBottomBun;
     public GameObject burgerTopBun;
-    public List<GameObject> burgerTicketList;
     public List<GameObject> burgerIngredientList;
     public Transform burgerSpawnLocation;
     public float burgerYOffset = -100f;
     public float burgerInitialOffset = -50f;
+    public float burgerXOffset = 200f;
+    public int maxBurgerTickets = 3;
+    public int amountOfBurgerTickets = 0;
 
     public GameObject pizzaDough;
     public GameObject pizzaSauce;
-    public List<GameObject> pizzaTicketList;
     public List<GameObject> pizzaIngredientList;
     public Transform pizzaSpawnLocation;
     public float pizzaYOffset = 100f;
     public float pizzaInitialOffset = 50f;
+    public float pizzaXOffset = 200f;
+    public int maxPizzaTickets = 3;
 
     public int difficulty = 1;
 
+    private List<List<GameObject>> burgerTickets = new List<List<GameObject>>();
+    private List<List<GameObject>> pizzaTickets = new List<List<GameObject>>();
+
     private void Start()
     {
-        UpdateBurgerTicket();
-        UpdatePizzaTicket();
+        UpdateBurgerTickets();
+        UpdatePizzaTickets();
+        StartCoroutine(Test());
     }
 
-    private void CreateBurgerTicket()
+    private IEnumerator Test()
     {
-        burgerTicketList = new List<GameObject>();
-        burgerTicketList.Add(burgerTopBun);
+        yield return new WaitForSeconds(5);
+        DeleteBurgerTicket(1);
+    }
 
+    private List<GameObject> CreateBurgerTicket()
+    {
+        List<GameObject> newTicket = new List<GameObject>();
+        GameObject topBun = Instantiate(burgerTopBun, burgerSpawnLocation);
+        newTicket.Add(topBun);
         int size = Random.Range(1, difficulty + 1);
         for (int i = 0; i < size; i++)
         {
             int choice = Random.Range(0, burgerIngredientList.Count);
-            burgerTicketList.Add(burgerIngredientList[choice]);
+            GameObject ingredient = Instantiate(burgerIngredientList[choice], burgerSpawnLocation);
+            newTicket.Add(ingredient);
         }
+        GameObject bottomBun = Instantiate(burgerBottomBun, burgerSpawnLocation);
+        newTicket.Add(bottomBun);
 
-        burgerTicketList.Add(burgerBottomBun);
+        return newTicket;
     }
 
-    private void UpdateBurgerTicket()
+    private void UpdateBurgerTickets()
     {
-        CreateBurgerTicket();
-        Vector3 spawnLocation = burgerSpawnLocation.position;
-
-        for (int i = 0; i < burgerTicketList.Count; i++)
+        if (amountOfBurgerTickets < maxBurgerTickets)
         {
-            GameObject spawnedObject = Instantiate(burgerTicketList[i], burgerSpawnLocation);
-            spawnedObject.transform.localPosition = new Vector3(0, i * burgerYOffset + burgerInitialOffset, 0);
+
+        }
+        for (int ticketIndex = 0; ticketIndex < maxBurgerTickets; ticketIndex++)
+        {
+            List<GameObject> ticket = CreateBurgerTicket();
+            burgerTickets.Add(ticket);
+
+            for (int i = 0; i < ticket.Count; i++)
+            {
+                ticket[i].transform.localPosition = new Vector3(
+                    ticketIndex * burgerXOffset,
+                    i * burgerYOffset + burgerInitialOffset,
+                    0
+                );
+            }
         }
     }
 
-    private void CreatePizzaTicket()
+    private List<GameObject> CreatePizzaTicket()
     {
-        pizzaTicketList = new List<GameObject>();
-        pizzaTicketList.Add(pizzaDough);
-        pizzaTicketList.Add(pizzaSauce);
-
+        List<GameObject> newTicket = new List<GameObject>();
+        GameObject dough = Instantiate(pizzaDough, pizzaSpawnLocation);
+        GameObject sauce = Instantiate(pizzaSauce, pizzaSpawnLocation);
+        newTicket.Add(dough);
+        newTicket.Add(sauce);
         int size = Random.Range(1, difficulty + 1);
         for (int i = 0; i < size; i++)
         {
             int choice = Random.Range(0, pizzaIngredientList.Count);
-            pizzaTicketList.Add(pizzaIngredientList[choice]);
+            GameObject topping = Instantiate(pizzaIngredientList[choice], pizzaSpawnLocation);
+            newTicket.Add(topping);
+        }
+
+        return newTicket;
+    }
+
+    private void UpdatePizzaTickets()
+    {
+        for (int ticketIndex = 0; ticketIndex < maxPizzaTickets; ticketIndex++)
+        {
+            List<GameObject> ticket = CreatePizzaTicket();
+            pizzaTickets.Add(ticket);
+
+            for (int i = 0; i < ticket.Count; i++)
+            {
+                ticket[i].transform.localPosition = new Vector3(
+                    ticketIndex * pizzaXOffset,
+                    i * pizzaYOffset + pizzaInitialOffset,
+                    0
+                );
+            }
         }
     }
 
-    private void UpdatePizzaTicket()
+    public void DeleteBurgerTicket(int index)
     {
-        CreatePizzaTicket();
-        Vector3 spawnLocation = pizzaSpawnLocation.position;
-
-        for (int i = 0; i < pizzaTicketList.Count; i++)
+        if (index >= 0 && index < burgerTickets.Count)
         {
-            GameObject spawnedObject = Instantiate(pizzaTicketList[i], pizzaSpawnLocation);
-            spawnedObject.transform.localPosition = new Vector3(0, i * pizzaYOffset + pizzaInitialOffset, 0);
+            List<GameObject> ticketToRemove = burgerTickets[index];
+            foreach (GameObject ingredient in ticketToRemove)
+            {
+                Destroy(ingredient);
+            }
+            burgerTickets.RemoveAt(index);
+            RearrangeBurgerTickets();
+        }
+    }
+
+    public void DeletePizzaTicket(int index)
+    {
+        if (index >= 0 && index < pizzaTickets.Count)
+        {
+            List<GameObject> ticketToRemove = pizzaTickets[index];
+            foreach (GameObject ingredient in ticketToRemove)
+            {
+                Destroy(ingredient);
+            }
+            pizzaTickets.RemoveAt(index);
+            RearrangePizzaTickets();
+        }
+    }
+
+    private void RearrangeBurgerTickets()
+    {
+        for (int ticketIndex = 0; ticketIndex < burgerTickets.Count; ticketIndex++)
+        {
+            List<GameObject> ticket = burgerTickets[ticketIndex];
+            for (int i = 0; i < ticket.Count; i++)
+            {
+                ticket[i].transform.localPosition = new Vector3(
+                    ticketIndex * burgerXOffset,
+                    i * burgerYOffset + burgerInitialOffset,
+                    0
+                );
+            }
+        }
+    }
+
+    private void RearrangePizzaTickets()
+    {
+        for (int ticketIndex = 0; ticketIndex < pizzaTickets.Count; ticketIndex++)
+        {
+            List<GameObject> ticket = pizzaTickets[ticketIndex];
+            for (int i = 0; i < ticket.Count; i++)
+            {
+                ticket[i].transform.localPosition = new Vector3(
+                    ticketIndex * pizzaXOffset,
+                    i * pizzaYOffset + pizzaInitialOffset,
+                    0
+                );
+            }
         }
     }
 }
