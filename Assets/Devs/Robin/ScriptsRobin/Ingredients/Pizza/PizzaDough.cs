2dff188587124c4ea77a3b84f06a029be6c173fa
DeanRBLagas <deanlagas@gmail.com>
2024-11-22 11:31:42 +0100

    Dough can also be handed in

--- Assets/Devs/Robin/ScriptsRobin/Ingredients/Pizza/PizzaDough.cs
+++ Assets/Devs/Robin/ScriptsRobin/Ingredients/Pizza/PizzaDough.cs
@@ -11,10 +11,13 @@ public class PizzaDough : MonoBehaviour
 
     [SerializeField]
     private Transform _spawn;
+
+    private PlayerMovement playerScript;
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            playerScript = other.GetComponent<PlayerMovement>();
             if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.UpArrow))
             {
                 Debug.Log("Hit player");
@@ -24,6 +27,7 @@ public class PizzaDough : MonoBehaviour
                     //doughObj.transform.localScale = new Vector3(0.02f, 0.0003f, 0.02f); // change its local scale in x y z format
                     //doughObj.transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
                     iscreated = true;
+                    playerScript.currentItems.Add(doughObj);
                 }
             }
         }
