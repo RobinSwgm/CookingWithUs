f3ee0984a7f9311378abdbf5e1b6406c2d60cb34
RobinSwgm <robin.swagerman@gmail.com>
2024-11-22 12:10:39 +0100

    overides

--- Assets/Devs/Robin/ScriptsRobin/Ingredients/Pizza/PizzaMain.cs
+++ Assets/Devs/Robin/ScriptsRobin/Ingredients/Pizza/PizzaMain.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class PizzaMain : MonoBehaviour
 {
-    public static int PickedupBase;
-    void Start()
-    {
-        
-    }
+   // [SerializeField] GameObject pizzaDoughPrefab;
 
-    void Update()
+    public static bool iscreated;
+
+   
+    public GameObject _parentRightHand;
+
+   
+    public Transform _spawn;
+
+    public PlayerMovement playerScript;
+
+
+    protected virtual void AdjustedSpawnPoint()
     {
-        
+
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerScript = other.GetComponent<PlayerMovement>();
+            if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.UpArrow))
+            {
+                Debug.Log("Hit player");
+                if (!iscreated)
+                {
+                    AdjustedSpawnPoint();
+                    //GameObject doughObj = Instantiate(pizzaDoughPrefab, _spawn.position, Quaternion.identity, _parentRightHand.transform);
+                    //doughObj.transform.localScale = new Vector3(0.02f, 0.0003f, 0.02f); // change its local scale in x y z format
+                    //doughObj.transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+                    iscreated = true;
+                   
+                }
+            }
+        }
     }
 }
--- Assets/Devs/Robin/ScriptsRobin/Ingredients/Pizza/PizzaSauce.cs
+++ Assets/Devs/Robin/ScriptsRobin/Ingredients/Pizza/PizzaSauce.cs
@@ -1,41 +1,47 @@
 using UnityEngine;
 
-public class PizzaSauce : MonoBehaviour
+public class PizzaSauce : PizzaMain
 {
     [SerializeField] GameObject pizzaSaucePrefab;
 
-    [SerializeField]
-    private GameObject _parentRightHand;
+   // private GameObject _parentRightHand;
 
-    private bool iscreated;
+    //private bool iscreated;
 
-    [SerializeField]
-    private Transform _spawn;
+    //[SerializeField]
+    //private Transform _spawn;
 
-    private PlayerMovement playerScript;
-    private void OnTriggerStay(Collider other)
+    //private PlayerMovement playerScript;
+
+    protected override void AdjustedSpawnPoint()
     {
-        if (other.CompareTag("Player"))
-        {
-            playerScript = other.GetComponent<PlayerMovement>();
-            if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.UpArrow))
-            {
-                if (!iscreated)
-                {
+        Vector3 adjustedSpawnPosition = _spawn.position + new Vector3(0f, 0.04f, 0f);
+        GameObject sauceObj = Instantiate(pizzaSaucePrefab, adjustedSpawnPosition, Quaternion.identity, _parentRightHand.transform);
+        playerScript.currentItems.Add(sauceObj);
 
-                    //zorgt ervoor dat de sauce boven de bodem van de pizza spawnt
-                    Vector3 adjustedSpawnPosition = _spawn.position + new Vector3(0f, 0.04f, 0f);
 
-                    //Debug.Log("Hit player");
-
-                    GameObject sauceObj = Instantiate(pizzaSaucePrefab, adjustedSpawnPosition, Quaternion.identity, _parentRightHand.transform);
-
-                   // sauceObj.transform.localScale = new Vector3(0.015f, 0.0003f, 0.015f); // change its local scale in x y z format
-                    //sauceObj.transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
-                    iscreated = true;
-                    playerScript.currentItems.Add(sauceObj);
-                }
-            }
-        }
     }
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        playerScript = other.GetComponent<PlayerMovement>();
+    //        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.UpArrow))
+    //        {
+    //            if (!iscreated)
+    //            {
+                   
+    //                //zorgt ervoor dat de sauce boven de bodem van de pizza spawnt
+    //               // Vector3 adjustedSpawnPosition = _spawn.position + new Vector3(0f, 0.04f, 0f);
+
+    //                //Debug.Log("Hit player");
+
+
+    //               // sauceObj.transform.localScale = new Vector3(0.015f, 0.0003f, 0.015f); // change its local scale in x y z format
+    //                //sauceObj.transform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+    //                iscreated = true;
+    //            }
+    //        }
+    //    }
+    //}
 }
